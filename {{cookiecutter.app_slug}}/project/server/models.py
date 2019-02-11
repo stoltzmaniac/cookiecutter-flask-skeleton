d9721d9efc42e401050434336980ecc08e36ab44
stoltzmaniac <scott@stoltzmanconsulting.com>
2019-02-11 08:19:07 -0700

    Refactor users into module

--- {{cookiecutter.app_slug}}/project/server/models.py
+++ {{cookiecutter.app_slug}}/project/server/models.py
@@ -1,42 +1,75 @@
 # project/server/models.py
 
 
-import datetime
-
-from flask import current_app
-
-from project.server import db, bcrypt
+from project.server import db
+from project.server.compat import basestring
 
 
-class User(db.Model):
+# Alias common SQLAlchemy names
+Column = db.Column
+relationship = db.relationship
 
-    __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    email = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    registered_on = db.Column(db.DateTime, nullable=False)
-    admin = db.Column(db.Boolean, nullable=False, default=False)
+def reference_col(tablename, nullable=False, pk_name="id", **kwargs):
+    """Column that adds primary key foreign key reference.
+    Usage: ::
+        category_id = reference_col('category')
+        category = relationship('Category', backref='categories')
+    """
+    return Column(
+        db.ForeignKey("{0}.{1}".format(tablename, pk_name)), nullable=nullable, **kwargs
+    )
 
-    def __init__(self, email, password, admin=False):
-        self.email = email
-        self.password = bcrypt.generate_password_hash(
-            password, current_app.config.get("BCRYPT_LOG_ROUNDS")
-        ).decode("utf-8")
-        self.registered_on = datetime.datetime.now()
-        self.admin = admin
 
-    def is_authenticated(self):
-        return True
+class CRUDMixin(object):
+    """Mixin that adds convenience methods for CRUD (create, read, update, delete) operations."""
 
-    def is_active(self):
-        return True
+    @classmethod
+    def create(cls, **kwargs):
+        """Create a new record and save it the database."""
+        instance = cls(**kwargs)
+        return instance.save()
 
-    def is_anonymous(self):
-        return False
+    def update(self, commit=True, **kwargs):
+        """Update specific fields of a record."""
+        for attr, value in kwargs.items():
+            setattr(self, attr, value)
+        return commit and self.save() or self
 
-    def get_id(self):
-        return self.id
+    def save(self, commit=True):
+        """Save the record."""
+        db.session.add(self)
+        if commit:
+            db.session.commit()
+        return self
 
-    def __repr__(self):
-        return "<User {0}>".format(self.email)
+    def delete(self, commit=True):
+        """Remove the record from the database."""
+        db.session.delete(self)
+        return commit and db.session.commit()
+
+
+class Model(CRUDMixin, db.Model):
+    """Base model class that includes CRUD convenience methods."""
+
+    __abstract__ = True
+
+
+class SurrogatePK(object):
+    """A mixin that adds a surrogate integer 'primary key' column named ``id`` to any declarative-mapped class."""
+
+    __table_args__ = {"extend_existing": True}
+
+    id = Column(db.Integer, primary_key=True)
+
+    @classmethod
+    def get_by_id(cls, record_id):
+        """Get record by ID."""
+        if any(
+            (
+                isinstance(record_id, basestring) and record_id.isdigit(),
+                isinstance(record_id, (int, float)),
+            )
+        ):
+            return cls.query.get(int(record_id))
+        return None
